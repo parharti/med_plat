056b25f5dd13285558391284706d8bd64c13a2c6
Prisha Sharma <140067623+parharti@users.noreply.github.com>
2025-07-11 15:04:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Change working directory to your Rasa project
+cd /app/rasa_project
+
 echo "Starting action server on port 5055..."
-rasa run actions --port 5055 &
+rasa run actions --port 5055 --actions actions &
 
 echo "Starting Rasa server on port ${PORT:-5005}..."
 rasa run \
   --enable-api \
   --port ${PORT:-5005} \
   --cors "*" \
-  --endpoints /app/rasa_project/endpoints.yml \
-  --model /app/rasa_project/models
+  --endpoints endpoints.yml \
+  --model models
